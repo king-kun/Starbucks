0a5c7528fd3f99d3e0880043a97beb52b1d25e85
Cayn <caiocarvalhofajardo@gmail.com>
2025-07-23 12:23:05 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".starbucks")
+
+function trocaCor(cor) {
+    circulo.style.background = cor
+}
+
+function trocaImagem(img){
+    imagem.src = img
+}
